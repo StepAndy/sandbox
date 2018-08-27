bfd67d258dca7292e633dc94e5cbaa538a93bb6d
Andrew Stepanov <stepanoff.work@gmail.com>
2018-08-27 14:45:39 +0700

    Update sandproject.c

--- plugins/sandproject.c
+++ plugins/sandproject.c
@@ -15,27 +15,3 @@ int	main(int argc, char const *argv[])
 	dlclose(lib);
 	return 0;
 }
-
-/*  int a = 5;
-	int b = 6;
-	int summm = summ(a,b);*/
-/*
-
-int	main(int argc, char const *argv[])
-{
-	int libfd = dlopen("./sanplugin.so",RTLD_LAZY); //Почему ворнинги на дексриптор? 
-	char *functionality = dlsym(libfd,"functionality");
-	printf("%s",functionality);
-	return 0;
-}
-*/
-
-/*
-int	main(int argc, char const *argv[])
-{
-	void (*handler)(char*) = dlsym(dlopen("./sanplugin.so",RTLD_LAZY),"printf_wrapper");
-	handler("functionality");
-	return 0; //Ошибка сегментирования (стек памяти сброшен на диск
-}
-
-*/
